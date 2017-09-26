8cc3dba78eb337501f5c5de2a40368b1eff6f65d
Grant <gsbackes@gmail.com>
2017-09-26 13:56:18 +0900

    Assignment 26: done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,14 @@ class Post < ApplicationRecord
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+
+  # List alphabetically
+  scope :ordered_by_title, -> { order('title ASC') }
+
+  # List in reverse-chrono order
+  scope :ordered_by_reverse_created_at, -> { order('created_at DESC') }
+
+
 	
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20 }, presence: true
